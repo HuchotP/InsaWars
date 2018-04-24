8531b3ba83b55f847f03d2e7db7586e27bce6352
Florian AYMARD <florian.aymard@gmail.com>
2018-04-24 21:25:26 +0200

    Initial commit

---
+++ src/CharacterWindow.java
@@ -0,0 +1,15 @@
+import javax.swing;
+
+public class CharacterWindow {
+
+  public CharacterWindow() {
+
+    JFrame frame = new JFrame("Insawars");
+
+    frame.setSize(400,800);
+    frame.setVisible(true);
+
+
+  }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+  public static void main(String[] args) {
+    CharacterWindow ch1 = new CharacterWindow();
+
+
+  }
+
+}
