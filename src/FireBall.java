e7e02e285caa620b9da8fa423e06e972fc8c1763
Adele <adele.prouvost@gmail.com>
2018-05-14 11:22:09 +0200

    ajout des commentaires pour toutes les classes filles attaques

--- src/FireBall.java
+++ src/FireBall.java
@@ -1,43 +1,42 @@
 
 
-import java.lang.Math; /* import la bibliothèque util*/
+import java.lang.Math; // import la bibliothèque lang
 
-public class FireBall extends Attack{  /* la classe FireBalle () L'attaque de la boule de feu) est une classe fille de la classe Attack qui englobe toute les attaques */
+public class FireBall extends Attack{  // la classe FireBalle ( L'attaque de la boule de feu) est une classe fille de la classe Attack qui englobe toute les attaques
 
-  /* Definition d'un nouveau GameManager */
+  // Definition d'un nouveau GameManager
   GameManager manager = new GameManager().getManager();
 
-  /* Constructeur */
+  // Constructeur
   public FireBall (Character c){
 
-    this.name= "Boule de feu"; /* indique le nom de l'attaque */
-    this.damage= 1.5*c.getStrength();  /* indique les dégats infligés à l'adversaire, en fonction de l'attribut force de l'attaquant */
-    this.dodgerate= 1.0/3; /* indique la probabilité pour l'adversaire d'esquiver l'attaque */
-    this.creditsRequired= 3; /* indique le nombre de crédits nécessaire pour l'attaquant afin d'effectuer l'attaque */
+    this.name= "Boule de feu"; // indique le nom de l'attaque
+    this.damage= 1.5*c.getStrength();  // indique les dégats infligés à l'adversaire, en fonction de l'attribut force de l'attaquant
+    this.dodgerate= 1.0/3; // indique la probabilité pour l'adversaire d'esquiver l'attaque
+    this.creditsRequired= 3; // indique le nombre de crédits nécessaire pour l'attaquant afin d'effectuer l'attaque
 
   }
   @Override
 
-  /* Méthode d'attaque ciblée sur une diréction séléctionée
-    Renvoie un booléen true si l'attaque a eu lieu, false si l'attaque n'a pas eu lieu */
+  // Méthode d'attaque ciblée sur une direction séléctionée. Renvoie un booléen true si l'attaque a eu lieu, false si l'attaque n'a pas eu lieu
 
   public boolean attack(){
 
-    Character c1= manager.getCharacter(manager.getTurn()); /* Character 1 est définit comme étant l'attaquant */
-    Character c2= manager.getCharacter(manager.getOppositeTurn()); /* Character 2 est définit comme étant l'adversaire  */
-    if ( c1.getCredits()>=this.creditsRequired){ /* verrification que l'attaquant possède suffisamment de crédits pour effectuer l'attaque*/
-      if( ( c1.getX() == c2.getX() && Math.abs(c1.getX()- c2.getX()) < 7 ) || (c1.getY() == c2.getY() && Math.abs( c1.getY()- c2.getY()) < 7 )){ /* l'adversaire doit se trouver dans une portée de 7 cases en ligne droite par rapport à l'attaquant*/
-        c2.takeDamage(this.damage, this.dodgerate);
-        return true; /* l'attaque a été effectuée */
+    Character c1= manager.getCharacter(manager.getTurn()); // Character 1 est définit comme étant l'attaquant
+    Character c2= manager.getCharacter(manager.getOppositeTurn()); // Character 2 est définit comme étant l'adversaire
+    if ( c1.getCredits()>=this.creditsRequired){ // verrification que l'attaquant possède suffisamment de crédits pour effectuer l'attaque
+      if( ( c1.getX() == c2.getX() && Math.abs(c1.getX()- c2.getX()) < 7 ) || (c1.getY() == c2.getY() && Math.abs( c1.getY()- c2.getY()) < 7 )){ // l'adversaire doit se trouver dans une portée de 7 cases en ligne droite par rapport à l'attaquant
+        c2.takeDamage(this.damage, this.dodgerate); // L'adervsaire subit des dégats
+        return true; // l'attaque a été effectuée
       }
     }
 
-    return false; /* L'attaque n'a pas été exécutée */
+    return false; // L'attaque n'a pas été exécutée
   }
 
   @Override
 
-  /* Affiche sur la fenetre de l'attaquant la portée et les différentes directions possibles de l'attaque */
+  // Affiche sur la fenetre de l'attaquant la portée et les différentes directions possibles de l'attaque
   public void paint(GameWindow gw, int x, int y) {
     gw.getWorld()[x][y].paintFireball();
   }
--- src/Heal.java
+++ src/Heal.java
@@ -1,38 +1,45 @@
 
 
-import java.lang.Math;
+import java.lang.Math; // import la bibliothèque lang
 
-public class Heal extends Attack{
+public class Heal extends Attack{ // la classe Heal ( L'attaque de soin) est une classe fille de la classe Attack qui englobe toute les attaques
 
+  // Definition d'un nouveau GameManager
   GameManager manager = new GameManager().getManager();
 
+  // Constructeur
   public Heal(Character c){
 
-    this.name= "Soins";
-    this.damage= 2*c.getIntel();
-    this.creditsRequired=4;
-    this.dodgerate=0;
+    this.name= "Soins";  // indique le nom de l'attaque
+    this.damage= 2*c.getIntel(); // indique les points de vie à ajouter en fonction de l'attribut Intelligence du personnage
+    this.creditsRequired=4; // indique le nombre de crédits nécessaire pour l'attaquant afin d'effectuer l'attaque
+    this.dodgerate=0;// indique la probabilité d'esquiver l'attaque
 
   }
 
   @Override
-  public boolean attack(){
-      Character c1 = manager.getCharacter(manager.getTurn());
-      if ( c1.getCredits()>=this.creditsRequired && c1.getLife() != c1.getMaxLife()){
 
-        if(this.damage >= c1.getMaxLife() - c1.getLife()) {
-          c1.healCharacter(c1.getMaxLife() - c1.getLife());
-          return true;
+  // Méthode d'attaque ciblée sur le personnage lui même pour se soigner d'un certain nombre de points de vie. Renvoie un booléen true si l'attaque a eu lieu, false si l'attaque n'a pas eu lieu
+
+  public boolean attack(){
+      Character c1 = manager.getCharacter(manager.getTurn()); // Character 1 est définit comme étant l'attaquant
+      if ( c1.getCredits()>=this.creditsRequired && c1.getLife() != c1.getMaxLife()){// verrification que l'attaquant possède suffisamment de crédits pour effectuer l'attaque
+
+        if(this.damage >= c1.getMaxLife() - c1.getLife()) { // Verrification que l'ajout de vie de permette pas de dépasser le niveau maximale de points de vie
+          c1.healCharacter(c1.getMaxLife() - c1.getLife()); // Le personnage est soigné jusqu'à la valeur maximale pour les points de vie
+          return true; // L'attaque a été effectuée
         }
 
-        c1.healCharacter(this.damage);
-        return true;
+        c1.healCharacter(this.damage); // L'attaquant est soigné
+        return true; // L'attaque a été effectuée
       } else{
-        return false;
+        return false; // L'attaque n'a pas été effectuée
       }
     }
 
   @Override
+
+  // Affiche sur la fenetre de l'attaquant la portée et les différentes directions possibles de l'attaque
   public void paint(GameWindow gw, int x, int y) {
     gw.getWorld()[x][y].paintHeal();
   }
