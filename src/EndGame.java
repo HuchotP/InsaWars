5b9edd4fbd3d51ae45e616d1994ac67dada19b92
unknown <faymard@PC106-025-08.insa-lyon.fr>
2018-05-14 11:26:13 +0200

    Ajout des commentaires

--- src/EndGame.java
+++ src/EndGame.java
@@ -7,41 +7,54 @@ import java.awt.event.ActionListener;
 
 public class EndGame extends JFrame {
 
-  //GameManager manager = new GameManager().getManager();
-  private int status = -1;
-  private JButton sameCharacter;
-  private JButton differentCharacter;
-  private JButton stop;
+  private int status = -1; // -1 : ne pas recommencer / 0 : recommencer avec les mêmes personnages / 1 : créer de nouveaux personnages
+  private JButton sameCharacter; // bouton pour recommencer avec les mêmes personnages
+  private JButton differentCharacter; // bouton pour créer de nouveaux personnages
+  private JButton stop; // bouton pour arrêter de jouer
 
   public EndGame(int n) {
 
+    // Mise en place de la fenêtre (titre, dimensions et évènement "bouton croix appuyé")
+
     this.setTitle("Fin du match");
     this.setSize(300,150);
-    Box box = Box.createVerticalBox();
-    JPanel pan = new JPanel();
-    //pan.setLayout(box);
+    this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE); // formel, n'influe pas sur le déroulement du programme et n'est pas utilisé
 
-    JLabel whowon = new JLabel("Le Joueur " + n + " a gagné la partie");
-    whowon.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-    sameCharacter = new JButton("Rejouer avec les mêmes personnages");
-    differentCharacter = new JButton("Jouer avec des personnages différents");
-    stop = new JButton("Arrêter de jouer");
+    // Création du panneau contenant tous les éléments
+
+    JPanel pan = new JPanel();
+
+    // Création du label annonçant le gagnant
+
+    JLabel whowon = new JLabel("Le Joueur " + n + " a gagn\u00e9 la partie"); // n valant 1 ou 2, selon le gagnant déduit par la fonction Manager.isFinished()
+    whowon.setAlignmentX(JComponent.CENTER_ALIGNMENT); // centrage du label
+
+    // Création des différents boutons
+
+    sameCharacter = new JButton("Rejouer avec les m\u00eames personnages");
+    differentCharacter = new JButton("Jouer avec des personnages diff\u00e9rents");
+    stop = new JButton("Arr\u00eater de jouer");
+
+    // Insertion des éléments dans le panneau (ordre d'ajout = ordre vertical d'apparition)
 
     pan.add(whowon);
     pan.add(sameCharacter);
     pan.add(differentCharacter);
     pan.add(stop);
+
+    // Ajout du panneau à la fenêtre et activation
+
     this.add(pan);
-    this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
     this.setVisible(true);
 
+    // Création des listeners pour les boutons
 
     sameCharacter.addActionListener(new ActionListener() {
 
       public void actionPerformed(ActionEvent e) {
 
-        status = 0;
-        setVisible(false);
+        status = 0; // on veut rejouer avec les mêmes personnages
+        setVisible(false); // on cache la fenêtre qui sera recréée à la fin de la prochaine partie
 
       }
 
@@ -52,8 +65,8 @@ public class EndGame extends JFrame {
 
       public void actionPerformed(ActionEvent e) {
 
-        status = 1;
-        setVisible(false);
+        status = 1; // on veut créer de nouveaux personnages
+        setVisible(false); // on cache la fenêtre qui sera recréée à la fin de la prochaine partie
 
       }
 
@@ -64,8 +77,8 @@ public class EndGame extends JFrame {
 
       public void actionPerformed(ActionEvent e) {
 
-        status = -1;
-        setVisible(false);
+        status = -1; // on ne veut pas rejouer
+        setVisible(false); // on cache la fenêtre et main() clôt le programme
 
       }
 
@@ -73,6 +86,10 @@ public class EndGame extends JFrame {
 
 
   }
+
+  /*
+  * @return : statut de la fenêtre expliqué au début du programme
+  */
 
   public int getStatus(){
 
